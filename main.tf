6ce1c8ff8fe5c29c77deca4df16e2969018aa7ef
Bryon Smith <webmastersmith37@gmail.com>
2022-07-27 15:23:48 -0500

    delete extra key block

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    host = module.eks.cluster_endpoint
+    host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = module.eks.cluster_certificate_authority_data
-     exec {
+    exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
       command     = "aws"
